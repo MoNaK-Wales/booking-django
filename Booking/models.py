caf10bb35c4287ed49e0423d78ee95ef2ae32c58
Maksim <felozan4ik2007@gmail.com>
2025-06-04 17:56:24 +0300

    room rename

--- Booking/models.py
+++ Booking/models.py
@@ -2,28 +2,28 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Room(models.Model):
-    number = models.IntegerField(unique=True)
+class BookingItem(models.Model):
+    title = models.CharField(max_length=100, unique=True)
     capacity = models.IntegerField()
 
     def __str__(self):
-        return f"Room №{self.number}"
+        return self.title
     
     class Meta:
-        ordering = ['number']
-        verbose_name = 'Room'
-        verbose_name_plural = 'Rooms'
+        ordering = ['capacity']
+        verbose_name = 'Booking Item'
+        verbose_name_plural = 'Booking Items'
     
 
 class Booking(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='bookings')
-    room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='bookings')
+    booking_item = models.ForeignKey(BookingItem, on_delete=models.CASCADE, related_name='bookings')
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Booking by {self.user.username} in {self.room}"
+        return f"Booking by {self.user.username} in {self.booking_item}"
     
     class Meta:
         ordering = ['start_time']
