13182b4c0fb4484c437afaf7bb9bb3753ea3c6af
Maksim <felozan4ik2007@gmail.com>
2025-07-30 18:03:16 +0200

    profile edit + footer

--- auth_users/forms.py
+++ auth_users/forms.py
@@ -4,3 +4,16 @@ from django import forms
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "password1", "password2"]
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ["username", "email"]
+        
--- auth_users/views.py
+++ auth_users/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate, logout
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, authenticate, logout, get_user_model
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import messages
+from .forms import CustomUserCreationForm, ProfileUpdateForm
 
 # Create your views here.
 def register(request):
@@ -9,14 +10,14 @@ def register(request):
         return redirect('main:locations')
 
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
             return redirect('main:locations')
     else:
-        form = UserCreationForm()
-    
+        form = CustomUserCreationForm()
+
     return render(request, 'auth_users/register.html', context={'form': form})
 
 def login_user(request):
@@ -42,6 +43,26 @@ def login_user(request):
 def logout_user(request):
     if not request.user.is_authenticated:
         return redirect('auth:login')
-    
+
     logout(request)
     return redirect('main:home')
+
+
+def edit_profile(request):
+    if not request.user.is_authenticated:
+        return redirect("auth:login")
+
+    user = get_user_model().objects.get(id=request.user.id)
+
+    if request.method == "POST":
+        form = ProfileUpdateForm(data=request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Профіль оновлено успішно.")
+            return redirect("main:profile")
+        else:
+            messages.error(request, "Неправильні дані профілю.")
+    else:
+        form = ProfileUpdateForm(instance=user)
+
+    return render(request, "auth_users/edit.html", context={"form": form})
